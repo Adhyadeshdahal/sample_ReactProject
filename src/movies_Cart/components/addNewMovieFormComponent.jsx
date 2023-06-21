d9f1648ef0dba4cd6318ff65ed50cf5238acc933
Adhyadeshdahal <adhyadeshdahal618@gmail.com>
2023-06-21 15:58:56 +0545

    remaining changes

--- src/movies_Cart/components/addNewMovieFormComponent.jsx
+++ src/movies_Cart/components/addNewMovieFormComponent.jsx
@@ -1,36 +1,46 @@
 import React from 'react'
 
-function AddNewMovieFormComponent() {
+function AddNewMovieFormComponent({genres,fields,forSubmit,val,forChange,error,activeId,isDisabled,forClick,repeatedMovie}) {
   return (
     <>
-    <form>
-  <div class="form-group">
-    <label for="Title">Title</label>
-    <input type="Text" className="form-control" id="Title" placeholder="In Time"/>
-  </div>
-  <div class="form-group">
-    <label for="Genre">Genre</label>
-    <select className="form-control" id="Genre">
-        {ogenres.map(obj=>{
+    <form onSubmit={forSubmit}>
+
+        {
+            fields.map(item =>{
+
+                return (
+                    <div key={item.id}>
+         <div className="form-group">
+            <label htmlFor={item.id}>{item.label}</label>
+            <input type={item.type} className="form-control" onChange ={forChange} onClick={forClick} id={item.id} 
+            placeholder={item.placeholder} value={val[item.id]}/>
+          </div>
+          {/* <div>{error[item.id]}</div> */}
+          <div className={(error[item.id]&&(activeId==item.id))?"alert alert-danger":"d-none"}>{error[item.id]}</div>
+          </div>
+)
+
+            })
+            
+        }
+
+  <div className="form-group">
+    <label htmlFor="genre">genre</label>
+    <select className="form-control" id="genre" onChange={forChange}>
+        {genres.map(obj=>{
             return(
                 <option key={obj._id}>{obj.name}</option>
 
             )
         })}
-      
       </select>
+      <div className={(error["genre"]&&(activeId=="genre"))?"alert alert-danger":"d-none"}>{error["genre"]}</div>
   </div>
-  <div class="form-group">
-    <label for="Stock">Number In Stock</label>
-    <input type="Text" className="form-control" id="stock" placeholder="6"/>
-  </div>
-  <div class="form-group">
-    <label for="Rate">Rate</label>
-    <input type="number" className="form-control" id="Rate" placeholder="2.5"/>
-  </div>
-  <button type="submit" className="btn btn-warning">Add</button>
+  <button type="submit" className="btn btn-warning" disabled={isDisabled}>Add</button>
 
   </form>
+
+  {repeatedMovie?(<div className="alert alert-danger">{"The Movie Already Exists In Database"}</div>):null}
 
     
     
